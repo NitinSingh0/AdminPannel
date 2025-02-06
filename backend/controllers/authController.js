56313f9ae5c578b0cf165a17269952e30b8bf5de
NitinSingh0 <31nitinthakur@gmail.com>
2025-02-06 15:14:02 +0530

    completed pole routes and controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,44 +1,60 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const Poll = require("../models/Poll");
 
 //Generate JWT token
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
 
 //Register User
 exports.registerUser = async (req, res) => {
-    const { fullName, username, email, password, profileImageUrl } = req.body;
+  const { fullName, username, email, password, profileImageUrl } = req.body;
 
-    //validation:check for missing field
-    if (!fullName || !username || !email || !password) {
-        return res.status(400).json({ message: "All fields are required" });
+  //validation:check for missing field
+  if (!fullName || !username || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  //Validation: Check username format
+  //Allows alphabet character and hyphen only
+  const usernameRegex = /^[a-zA-Z0-9-]+$/;
+  if (!usernameRegex.test(username)) {
+    return res.status(400).json({
+      message:
+        "Invalid username.Only alphanumeric characters and hyphens are allowed. No spaces are permitted.",
+    });
+  }
+  try {
+    //check if email alredy exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
     }
-    //Validation: Check username format
-    //Allows alphabet character and hyphen only
-    const usernameRegex = /^[a-zA-Z0-9-]+$/;
-    if (!usernameRegex.test(username)) {
-        return res.status(400).json({ message: "Invalid username.Only alphanumeric characters and hyphens are allowed. No spaces are permitted.", });
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res
+        .status(400)
+        .json({ message: "Username not available. Try another one" });
     }
-    try {
-        //check if email alredy exists 
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: "Email already in use" });
-        }
-        const existingUsername = await User.findOne({ username });
-        if (existingUsername) {
-            return res.status(400).json({ message: "Username not available. Try another one" });
-        }
 
-        //create the user
-        const user = await User.create({ fullName, username, email, password, profileImageUrl, });
-        res.status(201).json({id:user._id,user,token:generateToken(user._id),})
-    } catch (error) {
-        res.status(500).json({ message: "Error registering user", error: error.message });
-    }
-}
+    //create the user
+    const user = await User.create({
+      fullName,
+      username,
+      email,
+      password,
+      profileImageUrl,
+    });
+    res
+      .status(201)
+      .json({ id: user._id, user, token: generateToken(user._id) });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: error.message });
+  }
+};
 
 //Login User
 exports.loginUser = async (req, res) => {
@@ -46,42 +62,64 @@ exports.loginUser = async (req, res) => {
   //validation:check for missing field
   if (!email || !password) {
     return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(400).json({ message: "Invalid credentials" });
     }
-    
-    try {
-        const user = await User.findOne({ email });
-        if (!user || !(await user.comparePassword(password))) {
-            return res.status(400).json({ message: "Invalid credentials" });
-        }
-        res.status(200).json({
-            id: user._id, user: {
-            ...user.toObject(),totalPollsCreated : 0,totalPollsVotes : 0, totalPollsBookmarked : 0,
-            },
-        token: generateToken(user._id),
-        })
-    } catch (err) {
-        res
-          .status(500)
-          .json({ message: "Error logging in", error: err.message });
-    }
-}
+    //Count polls created by the user
+    const totalPollsCreated = await Poll.countDocuments({ creator: user._id });
+    //count polls the user has voted in
+    const totalPollsVotes = await Poll.countDocuments({
+      voters: user._id,
+    });
+    // Get the count of bookmarked polls
+    const totalPollsBookmarked = user.bookmarkedPolls.length;
+
+    res.status(200).json({
+      id: user._id,
+      user: {
+        ...user.toObject(),
+        totalPollsCreated,
+        totalPollsVotes,
+        totalPollsBookmarked,
+      },
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Error logging in", error: err.message });
+  }
+};
 
 //Get User info
 exports.getUserInfo = async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id).select("-password");
-        if (!user) {
-            return res.status(404).json({ message: "USer not found" });
-        }
-        //Add the new attribute to the response
-        const userInfo = {
-            ...user.toObject(),
-            totalPollsCreated:0,
-            totalPollsVotes:0,
-            totalPollsBookmarked:0,
-        };
-        res.status(200).json(userInfo);
-    } catch (err) {
-        res.status(500).json({ message: "Error registering user", error: err.message });
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "USer not found" });
     }
+
+    //Count polls created by the user
+    const totalPollsCreated = await Poll.countDocuments({ creator: user._id });
+    //count polls the user has voted in
+    const totalPollsVotes = await Poll.countDocuments({
+      voters: user._id,
+    });
+    // Get the count of bookmarked polls
+    const totalPollsBookmarked = user.bookmarkedPolls.length;
+    //Add the new attribute to the response
+    const userInfo = {
+      ...user.toObject(),
+      totalPollsCreated,
+      totalPollsVotes,
+      totalPollsBookmarked,
+    };
+    res.status(200).json(userInfo);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: err.message });
+  }
 };

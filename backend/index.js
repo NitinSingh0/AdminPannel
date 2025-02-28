d031676d35a5b12def48a4d368400ad58f8ac628
NitinSingh0 <31nitinthakur@gmail.com>
2025-03-01 01:25:51 +0530

    acess to all

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,7 @@ const connectDB = require("./config/db");
 const app = express();
 
 //middleware to handle CORS
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 connectDB();
